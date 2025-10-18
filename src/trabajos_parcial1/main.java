71ef65f9bd9e993ef2ec2de994c2295b5e12a5c7
goto9313X <chinogoto9313@gmail.com>
2025-10-18 02:44:54 -0600

    inicio de programa uno

---
+++ src/trabajos_parcial1/main.java
@@ -0,0 +1,11 @@
+package trabajos_parcial1;
+
+public class main {
+    public static void main (String[] args) {
+        int a[] = new int[1000];
+        a[0]=15;
+        System.out.println("vlaor"+a[0]);
+
+    }
+
+}
