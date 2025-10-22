d1197ad6d664e402302ebc1aab1c831b4dec9238
goto9313X <chinogoto9313@gmail.com>
2025-10-21 19:10:15 -0600

    se acaban la parte 1 y 2 y se agregan las 10 funciones de interfaces.

--- src/fes/aragon/utilerias/dinamicas/listasimple/main.java
+++ src/fes/aragon/utilerias/dinamicas/listasimple/main.java
@@ -2,6 +2,6 @@ package fes.aragon.utilerias.dinamicas.listasimple;
 import static fes.aragon.utilerias.dinamicas.listasimple.manejo_de_archivo.*;
 public class main {
     public static void main(String[] args) {
-       leerArchivo("C:\\Users\\chino\\Downloads\\trabajos-mike\\trabajosmike\\src\\trabajos_parcial1\\Archivo_CSV\\Datos.csv");
+       leerArchivo("src\\fes\\aragon\\utilerias\\dinamicas\\listasimple\\Datos.csv");
     }
 }
