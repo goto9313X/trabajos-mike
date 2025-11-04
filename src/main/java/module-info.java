3d6e4e73106263b3012ca9b9bc9ed375deb48573
goto9313X <chinogoto9313@gmail.com>
2025-11-03 20:37:23 -0600

    segundo parcial con interfaz grafica

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.segundo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.segundo to javafx.fxml;
+    exports com.example.segundo;
+}
